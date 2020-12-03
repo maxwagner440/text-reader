87bd9b99aa39d5f4c0bb412256048d0fc24b79d1
maxwagner440 <maxwagner440@gmail.com>
2020-12-03 14:30:01 -0500

    update

---
+++ src/models/constants.ts
@@ -0,0 +1,17 @@
+export const constants = {
+    backRN : /\r\n/,
+    newLineChar : '\r\n',
+    comma: ',',
+    period: '.',
+    allNonAlph: /\W/g,
+    backR: /\r/,
+    backN: /\n/,
+    backT: /\t/,
+    backB: /\b/,
+    backF: /\f/,
+    empty: '',
+    space: ' ',
+    one: 1,
+    sliceBeginZero: 0,
+    sliceEndTwenty: 20
+}
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,75 +1,76 @@
-import { WordCountObject } from "../models";
+import { WordCountObject, constants } from "../models";
 
 export class Parser {
   
   constructor() {}
 
   parseTextWordsBasedSpace(parseWordsString: string): string[] {
-    return parseWordsString.trim().split(" ");
+    return parseWordsString.trim().split(constants.space);
   }
 
   parseStopWordsBasedOnLineBreak(parseWordsString: string): string[] {
-    return parseWordsString.split("\n");
+    return parseWordsString.split(constants.newLineChar);
   }
 
   normalizeWords(textFromFileArray: string[]): string[] {
-    textFromFileArray.forEach(word => {
-      word = word.replace(',', '');
-      word = word.replace('.', '');
-      word = word.replace('\r', '');
-      word = word.toLowerCase().trim();
+    textFromFileArray.map(word => {
+      word = word.replace(constants.comma, constants.empty);
+      word = word.replace(constants.period, constants.empty);
+      word = word.replace(constants.allNonAlph, constants.empty);
+      word = word.replace(constants.newLineChar, constants.empty);
+      word = word.replace(constants.backRN, constants.empty);
+      word = word.replace(constants.backR, constants.empty)
+      word = word.replace(constants.backN, constants.empty)
+      word = word.replace(constants.backT, constants.empty)
+      word = word.replace(constants.backB, constants.empty)
+      word = word.replace(constants.backF, constants.empty)
+
+      word = word.toLowerCase();
     });
-    return textFromFileArray;
+     return textFromFileArray;
   }
 
   extractWordsFromText(stopWordsArray: string[], textFromFileArray: string[]): WordCountObject[]{
     let extractedWordsArray: WordCountObject[] = [];
     stopWordsArray.forEach((stopWord) => {
       textFromFileArray.forEach(word => {
-        let exists = false;
         if(stopWord.toLowerCase().trim() === word.toLowerCase()) {
-          extractedWordsArray.forEach(extractedWordObject => {
-            //if it exists in extractedWordsArray//
-            if(extractedWordObject.word.toLowerCase().trim() === word.toLowerCase()){
-              ++extractedWordObject.count
-              exists = true;
-            } 
-          });
-          if(exists === false) {
-             // //if doesn't exist, add with count of 1//
-            extractedWordsArray.push({
-              word: stopWord.toLowerCase(),
-              count: 1
-            });
-          }
+          extractedWordsArray = this.buildExtractedWordsArray(extractedWordsArray, word.toLowerCase());
         }
       });
     });
 
-    
-// stopWordsArray.forEach((stopWord) => {
-//   textFromFileArray.forEach(word => {
-//     if(stopWord.toLowerCase().trim() === word.toLowerCase()) {
-//       removedWordsArray.push(word.toLowerCase());
-//      }
-//   });
-// });
-
-
-// let removedWordsArrayWithCount: any[] = [];
-// removedWordsArray.forEach(word => {
-//   if(!Object.keys(removedWordsArrayWithCount).includes(word)){
-//     removedWordsArrayWithCount.push({
-//       word: {
-//         count: 1
-//       }
-//     });
-//   } else {
-//     ++removedWordsArrayWithCount[word].count
-//   }
-// });
-
     return extractedWordsArray;
+  }
+
+  countAllWordsFromText(textFromFileArray: string[]): WordCountObject[]{
+    let totalCountArray: WordCountObject[] = [];
+
+    textFromFileArray.forEach(word => {
+      totalCountArray = this.buildExtractedWordsArray(totalCountArray, word.toLowerCase());
+    });
+
+    return totalCountArray;
+  }
+
+  buildExtractedWordsArray(initialArray: WordCountObject[], stopWord: string): WordCountObject[] {
+    let exists = false;
+    initialArray.forEach(extractedWordObject => {
+      //if it exists in extractedWordsArray//
+      if(extractedWordObject.word.toLowerCase().trim() === stopWord.toLowerCase()){
+        ++extractedWordObject.count
+        exists = true;
+      } 
+    });
+    if(exists === false) {
+        // //if doesn't exist, add with count of 1//
+        initialArray.push({
+        word: stopWord.toLowerCase(),
+        count: constants.one
+      });
+    }
+
+    return initialArray;
   }
 
   orderArrayOnCountDesc(extractedWordsArray: WordCountObject[]): WordCountObject[] {
@@ -79,6 +80,6 @@ export class Parser {
   }
 
   showTwentyMostPrevelantWords(extractedWordsArray: WordCountObject[]): WordCountObject[] {
-    return extractedWordsArray.slice(0,20);
+    return extractedWordsArray.slice(constants.sliceBeginZero, constants.sliceEndTwenty);
   }
 }
